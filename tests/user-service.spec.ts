078f24dda10fa42e2953bceddbc0a1a3f4cd2c51
Shail <shail9689@gmail.com>
2025-01-03 21:58:43 +0530

    Added Tests

--- tests/user-service.spec.ts
+++ tests/user-service.spec.ts
@@ -1,3 +1,20 @@
-test("Initial test", () => {
-    expect(2).toEqual(2);
+describe('User Tests', () => {
+    let sampleVariable = 0;
+
+    beforeEach(() => {
+        sampleVariable++;
+        console.log(`Before Each. sampleVariable: ${sampleVariable}`);
+    });
+
+    test("First Test", () => {
+        expect(sampleVariable).toEqual(1);
+    });
+
+    test("Second Test", () => {
+        expect(sampleVariable).toEqual(2);
+    });
+
+    test("Third Test", () => {
+        expect(sampleVariable).toEqual(3);
+    });
 });
